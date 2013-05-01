b34a6717ece812d2817b6773f56198bab162b161
J. Aaron Eaton <aaron@channeleaton.com>
2013-05-01 08:15:19 -0500

    Fixed metabox references

--- AC/Metaboxes.php
+++ AC/Metaboxes.php
@@ -31,16 +31,142 @@ class AC_Metaboxes {
 		$meta_boxes = array();
 
 		$meta_boxes[] = array(
-			'id' => 'employee_details',
-			'title' => 'Employee Information',
-			'pages' => array( 'staff'),
+			'id' => 'submitter-information',
+			'title' => 'Submitter Information',
+			'pages' => array( 'location' ),
 			'context' => 'normal',
 			'priority' => 'high',
 			'fields' => array(
 				array(
-					'name' => 'First Name',
-					'id' => $prefix . 'first-name',
+					'name' => 'Submitter Name',
+					'id' => $prefix . 'submitter-name',
 					'type' => 'text',
+				),
+				array(
+					'name' => 'Submitter Email',
+					'id' => $prefix . 'submitter-email',
+					'type' => 'text',
+				),
+			),
+		);
+
+		$meta_boxes[] = array(
+			'id' => 'program-information',
+			'title' => 'Program Information',
+			'pages' => array( 'location' ),
+			'context' => 'normal',
+			'priority' => 'high',
+			'fields' => array(
+				array(
+					'name' => 'Program Title',
+					'id' => $prefix . 'program-title',
+					'type' => 'text',
+				),
+				array(
+					'name' => 'Description',
+					'id' => $prefix . 'program-description',
+					'type' => 'textarea',
+				),
+				array(
+					'name' => 'Program URL',
+					'id' => $prefix . 'program-url',
+					'type' => 'url',
+				),
+				array(
+					'name' => 'Department',
+					'id' => $prefix . 'program-department',
+					'type' => 'taxonomy',
+					'options' => array(
+						'taxonomy' => 'program-department',
+						'type' => 'select-tree',
+						'args' => array(),
+					),
+					'default' => 'Select a department',
+				),
+				array(
+					'name' => 'Street address (optional)',
+					'id' => $prefix . 'program-address',
+					'type' => 'text',
+				),
+				array(
+					'name' => 'City',
+					'id' => $prefix . 'program-city',
+					'type' => 'text',
+				),
+				array(
+					'name' => 'State (optional)',
+					'id' => $prefix . 'program-state',
+					'type' => 'text',
+				),
+				array(
+					'name' => 'Country',
+					'id' => $prefix . 'program-country',
+					'type' => 'text',
+				),
+				array(
+					'name' => 'Region',
+					'id' => $prefix . 'program-region',
+					'type' => 'taxonomy',
+					'options' => array(
+						'taxonomy' => 'program-region',
+						'type' => 'select-tree',
+						'args' => array(),
+					),
+					'default' => 'Select a region',
+				),
+				array(
+					'name' => 'Category',
+					'id' => $prefix . 'program-category',
+					'type' => 'taxonomy',
+					'options' => array(
+						'taxonomy' => 'program-category',
+						'type' => 'select_tree',
+						'args' => array(),
+					),
+					'default' => 'Select a category',
+				),
+			),
+		);
+
+		$meta_boxes[] = array(
+			'id' => 'study-abroad',
+			'title' => 'For Study Abroad/Student Experience Only',
+			'pages' => array( 'location' ),
+			'context' => 'normal',
+			'priority' => 'high',
+			'fields' => array(
+				array(
+					'name' => 'Time offered',
+					'id' => $prefix . 'program-time',
+					'type' => 'taxonomy',
+					'options' => array(
+						'taxonomy' => 'time-offered',
+						'type' => 'select-tree',
+						'args' => array(),
+					),
+					'multiple' => true,
+				),
+				array(
+					'name' => 'Type',
+					'id' => $prefix . 'program-type',
+					'type' => 'taxonomy',
+					'options' => array(
+						'taxonomy' => 'program-type',
+						'type' => 'select-tree',
+						'args' => array(),
+					),
+					'multiple' => true,
+				),
+				array(
+					'name' => 'Major',
+					'id' => $prefix . 'program-major',
+					'type' => 'taxonomy',
+					'options' => array(
+						'taxonomy' => 'program-major',
+						'type' => 'select-tree',
+						'args' => array(),
+					),
+					'multiple' => true,
 				),
 			),
 		);
