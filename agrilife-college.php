9fe41549cb0a2dd9adb9d4681c7df7c4d9235000
J. Aaron Eaton <aaron@channeleaton.com>
2013-05-10 16:33:00 -0500

    Added beta survey notification

--- agrilife-college.php
+++ agrilife-college.php
@@ -45,6 +45,8 @@ class AgriLife_College {
 
 		add_filter( 'title_save_pre', array( $this, 'save_title' ) );
 
+	  add_action( 'wp_enqueue_scripts', array( $this, 'agrilife_add_beta_alert' ) );
+
 	}
 
 	public function init() {
@@ -339,6 +341,20 @@ class AgriLife_College {
   	return $cpt;
 
   }
+
+  /**
+   * Adds the beta survey alert.
+   * REMOVE AFTER OFFICIAL LAUNCH
+   * @return [type] [description]
+   */
+	public function agrilife_add_beta_alert() {
+		wp_enqueue_script( 'beta-alert',
+			plugin_dir_url( __FILE__ ) . '/js/beta-alert.js',
+			'',
+			false,
+			true
+		);
+	}
 
 	public static function autoload( $classname ) {
 
