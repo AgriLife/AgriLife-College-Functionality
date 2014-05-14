21257e05071e3ae307fe7b3b35b4a542ad77b6c9
J. Aaron Eaton <aaron@channeleaton.com>
2014-05-14 17:58:30 -0500

    DOM & CSS changes

--- AC/Shortcode/GrandChallengesPeople.php
+++ AC/Shortcode/GrandChallengesPeople.php
@@ -19,15 +19,22 @@ class AC_Shortcode_GrandChallengesPeople {
 		AC_Ajax::set_ajax_url();
 
 		ob_start(); ?>
-		<div id="gc-list">
-			<div class="gc-selection">
-				<ul>
-					<li data-challenge="Food">Feeding Our World</li>
-					<li data-challenge="Environment">Protecting Our Environment</li>
-					<li data-challenge="Health">Improving Our Health</li>
-					<li data-challenge="Youth">Enriching Our Youth</li>
-					<li data-challenge="Economy">Growing Our Economy</li>
+		<div class="gc-list">
+			<div class="people-search-form">
+				<label>
+					<h4>Search faculty by specialty</h4>
+				</label>
+				<ul class="button-group radius">
+					<li data-challenge="Food"><a href="#" class="button secondary">Feeding Our World</a></li>
+					<li data-challenge="Environment"><a href="#" class="button secondary">Protecting Our Environment</a></li>
+					<li data-challenge="Health"><a href="#" class="button secondary">Improving Our Health</a></li>
+					<li data-challenge="Youth"><a href="#" class="button secondary">Enriching Our Youth</a></li>
+					<li data-challege="Economy"><a href="#" class="button secondary">Growing Our Economy</a></li>
 				</ul>
+				<form role="search" class="people-searchform" method="get" id="searchform" action="http://local.wordpress.dev/college/">
+					<input type="text" class="s" name="s" id="s" placeholder="biochemistry" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/><br />
+					<input type="hidden" name="post_type" value="people" />
+				</form>
 			</div>
 			<ul id="people-listing-ul"></ul>
 		</div>
@@ -45,17 +52,17 @@ class AC_Shortcode_GrandChallengesPeople {
 					</div>
 					<div class="people-contact-details">
 						<p class="people-email email"><a href="mailto:<%= emailaddress %>"><%= _.escape(emailaddress) %></a></p>
-						<% if (specializations != false) { %>
-							<h4>Specialties</h4>
-							<div class="people-specialty-list expanded">
-								<ul>
-									<% _.each(specializations, function(sp) { %>
-										<li><%= sp %></li>
-									<% }); %>
-								</ul>
-							</div>
-						<% } %>
+						<p class="people-specialty"><a href="#">Specializations</a></p>
 					</div>
+					<% if (specializations != false) { %>
+						<div class="people-specialty-list expanded">
+							<ul>
+								<% _.each(specializations, function(sp) { %>
+									<li><%= sp %></li>
+								<% }); %>
+							</ul>
+						</div>
+					<% } %>
 				</div>
 			</li>
 		</script>
