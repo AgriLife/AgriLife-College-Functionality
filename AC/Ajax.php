e94c4b388a10ffc114a8561385873579755fbbeb
J. Aaron Eaton <aaron@channeleaton.com>
2014-05-14 13:41:07 -0500

    Added AJAX method to get college faculty & staff

--- AC/Ajax.php
+++ AC/Ajax.php
@@ -11,6 +11,9 @@ class AC_Ajax {
 		add_action( 'wp_ajax_get_locations', 'AC_Ajax::get_locations' );
 		add_action( 'wp_ajax_nopriv_get_locations', 'AC_Ajax::get_locations' );
 
+		add_action( 'wp_ajax_get_people', 'AC_Ajax::get_people' );
+		add_action( 'wp_ajax_nopriv_get_people', 'AC_Ajax::get_people' );
+
 	}
 
 	public static function set_ajax_url() {
@@ -32,6 +35,23 @@ class AC_Ajax {
 		);
 
 		wp_localize_script( 'location-map', 'data', $data );
+
+	}
+
+	public static function get_people() {
+
+		$agrilife_people = get_transient( 'agrilife_people' );
+
+		if ( false === $agrilife_people ) {
+			// Get from PeopleAPI
+			include plugin_dir_path( dirname( dirname(__FILE__) ) ) . '/agrilife-core/src/PeopleAPI.php';
+			$soap = new \SoapClient( 'https://agrilifepeople-beta.tamu.edu/api/v4.cfc?wsdl' );
+			$api = new \AgriLife\Core\PeopleAPI( $soap );
+			$agrilife_people = $api->get_people( AGRILIFE_GET_PERSONNEL_HASH, array( 294, 286, 291, 379, 290, 297, 292, 300, 366, 298, 295, 396, 314, 302, 304 ) );
+			set_transient( 'agrilife_people', $agrilife_people, WEEK_IN_SECONDS );
+		}
+
+		die( json_encode( $agrilife_people ) );
 
 	}
 
