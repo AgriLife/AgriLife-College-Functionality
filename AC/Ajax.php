8070ad93ec84a05946e1cb66ea41f989d7f55e60
Zachary Watkins <Zachary.Watkins@ag.tamu.edu>
2015-08-24 14:18:26 -0500

    Update AgriLife People API to v4.

--- AC/Ajax.php
+++ AC/Ajax.php
@@ -46,7 +46,18 @@ class AC_Ajax {
 		if ( false === $agrilife_people ) {
 			// Get from PeopleAPI
 			include plugin_dir_path( dirname( dirname(__FILE__) ) ) . '/agrilife-core/src/PeopleAPI.php';
-			$soap = new \SoapClient( 'https://agrilifepeople.tamu.edu/api/v4.cfc?wsdl' );
+			$soap = new \SoapClient("https://agrilifepeople-api.tamu.edu/api/v4.cfc?wsdl", array(
+        'trace' => 1, 
+        'exception' => 0,
+        'encoding' => 'UTF-8',
+        'stream_context' => stream_context_create(array(
+          'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => false,
+          )
+        ))
+      ));
 			$api = new \AgriLife\Core\PeopleAPI( $soap );
 			$cached = false;
 			$agrilife_people = $api->get_people( AGRILIFE_GET_PERSONNEL_HASH, array( 294, 286, 291, 379, 290, 297, 292, 300, 366, 298, 295, 396, 314, 302, 304, 329, 353, 354, 355, 356, 357, 358, 359, 361, 362, 363, 364, 365 ) );
